2d1c56d188789e4e872d0c551433d11a318e7526
ArchLeaders <archleadership28@gmail.com>
2026-07-08 14:46:25 -0700

    refactor: Base BreadcrumbBar on ItemsControl with BreadcrumbBarItems

--- Avalonia.BreadcrumbBar/BreadcrumbBar.cs
+++ Avalonia.BreadcrumbBar/BreadcrumbBar.cs
@@ -1,25 +1,16 @@
-using System.Collections;
 using Avalonia.Controls;
-using Avalonia.Controls.Primitives;
-using Avalonia.Controls.Templates;
 
 namespace Avalonia.BreadcrumbBar;
 
-public sealed class BreadcrumbBar : TemplatedControl
+public sealed class BreadcrumbBar : ItemsControl
 {
-    public static readonly StyledProperty<IEnumerable?> ItemsSourceProperty =
-        AvaloniaProperty.Register<ItemsControl, IEnumerable?>(nameof(ItemsSource));
-
-    public static readonly StyledProperty<IDataTemplate?> ItemTemplateProperty =
-        AvaloniaProperty.Register<ItemsControl, IDataTemplate?>(nameof(ItemTemplate));
-
-    public IEnumerable? ItemsSource {
-        get => GetValue(ItemsSourceProperty);
-        set => SetValue(ItemsSourceProperty, value);
+    protected override Control CreateContainerForItemOverride(object? item, int index, object? recycleKey)
+    {
+        return new BreadcrumbBarItem();
     }
-
-    public IDataTemplate? ItemTemplate {
-        get => GetValue(ItemTemplateProperty);
-        set => SetValue(ItemTemplateProperty, value);
+    
+    protected override bool NeedsContainerOverride(object? item, int index, out object? recycleKey)
+    {
+        return NeedsContainer<BreadcrumbBarItem>(item, out recycleKey);
     }
 }
---
+++ Avalonia.BreadcrumbBar/BreadcrumbBarItem.cs
@@ -0,0 +1,75 @@
+using System.Windows.Input;
+using Avalonia.Controls;
+using Avalonia.Controls.Metadata;
+using Avalonia.Controls.Presenters;
+using Avalonia.Controls.Primitives;
+using Avalonia.Controls.Templates;
+using Path = Avalonia.Controls.Shapes.Path;
+
+namespace Avalonia.BreadcrumbBar;
+
+[TemplatePart("PART_Button", typeof(Button))]
+[TemplatePart("PART_Icon", typeof(ContentPresenter))]
+[TemplatePart("PART_ContentPresenter", typeof(ContentPresenter))]
+[TemplatePart("PART_ChevronIcon", typeof(Viewbox))]
+[TemplatePart("PART_ChevronPath", typeof(Path))]
+[PseudoClasses(":hasflyout")]
+public sealed class BreadcrumbBarItem : ContentControl
+{
+    public static readonly StyledProperty<object?> IconProperty =
+        AvaloniaProperty.Register<BreadcrumbBarItem, object?>(nameof(Icon));
+
+    public static readonly StyledProperty<Thickness> IconPaddingProperty =
+        AvaloniaProperty.Register<BreadcrumbBarItem, Thickness>(nameof(IconPadding));
+
+    public static readonly StyledProperty<IDataTemplate?> IconTemplateProperty =
+        AvaloniaProperty.Register<BreadcrumbBarItem, IDataTemplate?>(nameof(IconTemplate));
+
+    public static readonly StyledProperty<FlyoutBase?> FlyoutProperty =
+        AvaloniaProperty.Register<BreadcrumbBarItem, FlyoutBase?>(nameof(Flyout));
+
+    public static readonly StyledProperty<ICommand?> CommandProperty =
+        AvaloniaProperty.Register<BreadcrumbBarItem, ICommand?>(nameof(Command), enableDataValidation: true);
+
+    public static readonly StyledProperty<object?> CommandParameterProperty =
+        AvaloniaProperty.Register<BreadcrumbBarItem, object?>(nameof(CommandParameter));
+
+    public object? Icon {
+        get => GetValue(IconProperty);
+        set => SetValue(IconProperty, value);
+    }
+
+    public Thickness IconPadding {
+        get => GetValue(IconPaddingProperty);
+        set => SetValue(IconPaddingProperty, value);
+    }
+
+    public IDataTemplate? IconTemplate {
+        get => GetValue(IconTemplateProperty);
+        set => SetValue(IconTemplateProperty, value);
+    }
+
+    public FlyoutBase? Flyout {
+        get => GetValue(FlyoutProperty);
+        set => SetValue(FlyoutProperty, value);
+    }
+
+    public ICommand? Command {
+        get => GetValue(CommandProperty);
+        set => SetValue(CommandProperty, value);
+    }
+
+    public object? CommandParameter {
+        get => GetValue(CommandParameterProperty);
+        set => SetValue(CommandParameterProperty, value);
+    }
+
+    protected override void OnPropertyChanged(AvaloniaPropertyChangedEventArgs change)
+    {
+        base.OnPropertyChanged(change);
+
+        if (change.Property == FlyoutProperty) {
+            PseudoClasses.Set(":hasflyout", change.NewValue is not null);
+        }
+    }
+}
