b3f7bd4b5a3332aae3b3e8f9c644ebf27e5a2fe3
ArchLeaders <archleadership28@gmail.com>
2026-07-08 16:49:13 -0700

    chore: Update sample app

--- Samples/SampleApp/ViewModels/MainWindowViewModel.cs
+++ Samples/SampleApp/ViewModels/MainWindowViewModel.cs
@@ -6,6 +6,8 @@ namespace SampleApp.ViewModels;
 public sealed class MainWindowViewModel
 {
     public IEnumerable<Crumb> Items => [
-        new("Home", "fa-solid fa-house")
+        new("Home", "fa-solid fa-house"),
+        new("Projects", "fa-solid fa-cube"),
+        new("Documents", "fa-solid fa-file"),
     ];
 }
